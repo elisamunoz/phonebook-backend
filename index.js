81277fc82d2f7ba4620671b243197731dc332891
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-08-28 12:30:33 +0200

    3.7

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan(
+    ':method - :status - :url"HTTP/:http-version" - :response-time ms - :date[clf]'
+  )
+);
 
 let persons = [
   {
